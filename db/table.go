6b62402b725da1511036be7d40fb30250621d70a
insomnius <aliana.arib@gmail.com>
2024-11-28 00:34:45 +0700

    feat: db table update

--- db/table.go
+++ db/table.go
@@ -15,24 +15,16 @@ type Table struct {
 func (t *Table) FindByID(id string) (any, error) {
 	var v any
 	var found bool
-	err := t.enqueueProcess(func(*Instance) error {
-		// handling read commited
-		if changeV, ok := t.changes[id]; ok {
-			v = changeV
-			found = true
-			return nil
-		}
+	if changeV, ok := t.changes[id]; ok {
+		v = changeV
+		found = true
+		return v, nil
+	}
 
-		// read uncommitted
-		v, found = t.data[id]
-		if !found {
-			return ErrNotFound
-		}
-
-		return nil
-	}, "findById")
-	if err != nil {
-		return nil, err
+	// read uncommitted
+	v, found = t.data[id]
+	if !found {
+		return nil, ErrNotFound
 	}
 
 	return v, nil
@@ -41,21 +33,17 @@ func (t *Table) FindByID(id string) (any, error) {
 func (t *Table) Filter(f func(v any) bool) []any {
 	filtered := []any{}
 
-	_ = t.enqueueProcess(func(*Instance) error {
-		for key, value := range t.data {
-			// handling read commited
-			if changeV, ok := t.changes[key]; ok {
-				value = changeV
-			}
-
-			// read uncommitted
-			if f(value) {
-				filtered = append(filtered, value)
-			}
+	for key, value := range t.data {
+		// handling read commited
+		if changeV, ok := t.changes[key]; ok {
+			value = changeV
 		}
 
-		return nil
-	}, "filter")
+		// read uncommitted
+		if f(value) {
+			filtered = append(filtered, value)
+		}
+	}
 
 	return filtered
 }
